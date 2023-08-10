02f3e842634971c85b1b70a6bf70674a7a831037
Malin Freeborn <malinfreeborn@posteo.net>
2023-08-10 23:24:52 +0200

    create tlmgr deps installation script

---
+++ tlmgrDeps.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+# This file has the recommended LaTeX packages.
+# First install tlmgr (likely in the package 
+# texlive or texlive-bin).
+
+tlmgr install latex-bin
+
+tlmgr install \
+xcolor \
+gfsartemisia \
+epsdice \
+fontenc \
+starfont \
+appendix \
+geometry \
+svg \
+alltt \
+tabularx \
+wrapfig \
+epigraph \
+microtype \
+float \
+microtype \
+makeidx \
+babel \
+colortbl \
+contour \
+amssymb \
+amsmath \
+amsfonts \
+etoolbox \
+hyperref \
+pdftexcmds.sty \
+infwarerr.sty \
+glossaries \
+glossaries-extra \
+euler \
+koma-script \
+trimspaces \
+ifplatform \
+catchfile \
+kvoptions \
+transparent \
+nextpage \
+markdown \
+pgf \
+paralist \
+csvsimple \
+fancyvrb \
+babel-english \
+gobble \
+titlesec \
+fancyhdr \
+fourier \
+needspace \
+psnfss \
+tcolorbox \
+visualtikz \
+tikzfill \
+pdfcol \
+epstopdf-pkg \
+environ \
+collection-fontrecommended
